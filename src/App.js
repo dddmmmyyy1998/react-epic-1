4fed1cc48c67026cefd4400161918574447c3cec
mengya <1078282553@qq.com>
2021-08-31 10:35:59 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import React, { Suspence, lazy } from 'react';
+
+
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Loading from './components/Loading';
+import {Switch,Route} from 'react-router-dom'
+
+
+// import Home from './pages/Home';
+// import History from './pages/History';
+// import About from './pages/About';
+
+const Home = lazy(() => import('./pages/Home'));
+const History = lazy(() => import('./pages/History'));
+const About = lazy(() => import('./pages/About'));
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Suspence fallback={Loading}>
+        <Switch>
+          {/* 一条路由 */}
+          <Route path="/" exact component={Home} />
+          <Route path="/history" component={History} />
+          <Route path="/About" component={About} />
+        </Switch>
+      </Suspence>
+      <Footer />
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
 
+
+console.log(App)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
